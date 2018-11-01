c7e5d3ad5d34d4ac79f3b79c0ec3a906909f1717
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2018-11-02 00:45:13 +0600

    Database seeder added

---
+++ database/factories/FilmFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Film::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'description' => $faker->text,
+        'release_date' => $faker->date(),
+        'rating' => $faker->numberBetween(1,5),
+        'ticket_price' => $faker->randomNumber(),
+        'country' => $faker->country,
+        'genre' => $faker->randomElement(['Comedy','Drama','Horror','Action']),
+        'photo' => $faker->image('public/image',400,300, null, false)
+    ];
+});
